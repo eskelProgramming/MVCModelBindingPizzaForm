d0dd3bb3020fda784b68e6ba9fd2c82d0dbb74d1
eskelProgramming <eskelley0673@students.cptc.edu>
2024-07-16 15:04:46 -0700

    Created Pizza controller

---
+++ MVCModelBindingPizzaForm/Controllers/PizzaController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCModelBindingPizzaForm.Controllers
+{
+    public class PizzaController : Controller
+    {
+        [HttpGet]
+        public IActionResult Pizza()
+        {
+            return View();
+        }
+    }
+}
